60cc5e55e88ffd4cefeac3c4fdf39055b4a98ae1
vityooook <vityooook@gmail.com>
2025-01-24 21:01:09 +0300

    first stage ready

--- tests/Lottery.spec.ts
+++ tests/Lottery.spec.ts
@@ -5,24 +5,52 @@ import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
 
 describe('Lottery', () => {
-    let code: Cell;
 
-    beforeAll(async () => {
-        code = await compile('Lottery');
-    });
+    
+    let code: Cell;
 
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
+    let user1: SandboxContract<TreasuryContract>;
+    let user2: SandboxContract<TreasuryContract>;
+    let user3: SandboxContract<TreasuryContract>;
     let lottery: SandboxContract<Lottery>;
+    
 
-    beforeEach(async () => {
+    beforeAll(async () => {
+        code = await compile('Lottery');
+
         blockchain = await Blockchain.create();
+        
+        // blockchain.verbosity = {
+        //     print: true,
+        //     blockchainLogs: false,
+        //     vmLogs: "vm_logs",
+        //     // vmLogs: "none",
+        //     debugLogs: true
+        // }
 
-        lottery = blockchain.openContract(Lottery.createFromConfig({}, code));
 
         deployer = await blockchain.treasury('deployer');
+        user1 = await blockchain.treasury('user1');
+        user2 = await blockchain.treasury('user2');
+        user3 = await blockchain.treasury('user3');
 
-        const deployResult = await lottery.sendDeploy(deployer.getSender(), toNano('0.05'));
+        lottery = blockchain.openContract(Lottery.createFromConfig({
+            admin: deployer.address,
+            gameTime: 60,
+            minBet: toNano("0.25"),
+            maxBet: toNano("100"),
+            maxParticipates: 100
+        }, code));
+
+        await blockchain.setVerbosityForAddress(lottery.address, {
+            blockchainLogs: false,
+            vmLogs: 'vm_logs',
+            debugLogs: true
+        })
+
+        const deployResult = await lottery.sendDeploy(deployer.getSender(), toNano('1'));
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
@@ -31,9 +59,32 @@ describe('Lottery', () => {
             success: true,
         });
     });
+    // beforeEach(async () => {
+        
+    // });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and lottery are ready to use
+    it('three more bet', async () => {
+
+        const message = await lottery.sendBet(user1.getSender(), toNano("0.25"));
+        await lottery.sendBet(user2.getSender(), toNano("2"));
+        await lottery.sendBet(user3.getSender(), toNano("10"));
+
+        expect(message.transactions).toHaveTransaction({
+            from: user1.address,
+            to: lottery.address,
+            success: true
+        })
+
+    });
+
+    it("start game", async () => {
+        const message = await lottery.sendStartLottery({game_number: 0});
+
+        expect(message.transactions).toHaveTransaction({
+            from: lottery.address,
+            to: lottery.address,
+            success: true
+        })
+
     });
 });
--- wrappers/Lottery.ts
+++ wrappers/Lottery.ts
@@ -1,9 +1,31 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, Dictionary } from '@ton/core';
 
-export type LotteryConfig = {};
+export type LotteryConfig = {
+    admin: Address;
+    gameTime: number;
+    minBet: bigint;
+    maxBet: bigint;
+    maxParticipates: number;
+
+
+};
 
 export function lotteryConfigToCell(config: LotteryConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeDict(Dictionary.empty(), Dictionary.Keys.BigUint(64), Dictionary.Values.Cell())
+        .storeCoins(0)
+        .storeAddress(config.admin)
+        .storeUint(config.gameTime, 16)
+        .storeCoins(config.minBet)
+        .storeCoins(config.maxBet)
+        .storeUint(config.maxParticipates, 8)
+        .storeUint(0, 8)
+        .storeUint(0, 16)
+        .storeInt(0, 2)
+        .storeUint(0, 32)
+        .storeUint(0, 32)
+        .storeAddress(null)
+    .endCell();
 }
 
 export class Lottery implements Contract {
@@ -25,5 +47,32 @@ export class Lottery implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendBet(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+
+    async sendStartLottery(
+        provider: ContractProvider,
+        opts: {
+            game_number: number;
+        }
+    ) {
+       
+        await provider.external(
+            beginCell()
+                .storeRef(
+                    beginCell()
+                        .storeUint(opts.game_number, 32)
+                    .endCell()
+                )
+            .endCell()
+        );
     }
 }
